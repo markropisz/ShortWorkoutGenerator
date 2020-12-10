262809fd8e9839cff5775d66d8e1356c14964410
markropisz <markropisz@gmail.com>
2020-12-10 13:28:20 +0100

    Adding "export default ... to PdfButton

--- WorkoutGenerator/frontend/src/components/PdfButton.js
+++ WorkoutGenerator/frontend/src/components/PdfButton.js
@@ -1,34 +1,7 @@
 import React from 'react';
 import './index.css';
 import { Button } from 'antd';
-import jsPDF from 'jspdf';  
-import html2canvas from 'html2canvas';  
-
-export class MatTable extends Component {  
-    constructor(props) {  
-      super(props)  
-      this.state = {  
-        ProductData: []  
-    
-      }  
-    }  
-    printDocument() {  
-      const input = document.getElementById('pdfdiv');  
-      html2canvas(input)  
-        .then((canvas) => {  
-          var imgWidth = 200;  
-          var pageHeight = 290;  
-          var imgHeight = canvas.height * imgWidth / canvas.width;  
-          var heightLeft = imgHeight;  
-          const imgData = canvas.toDataURL('image/png');  
-          const pdf = new jsPDF('p', 'mm', 'a4')  
-          var position = 0;  
-          var heightLeft = imgHeight;  
-          pdf.addImage(imgData, 'JPEG', 0, position, imgWidth, imgHeight);  
-          pdf.save("download.pdf");  
-        });  
-    }
-
+import { CSVLink } from "react-csv"; 
 
 
 
@@ -36,10 +9,12 @@ class PdfButton extends React.Component {
     render() {
         return(
             <Button
-                onClick={}
+                // onClick={}
             >
-                Extract a PDF
+                Extract a list of chosen exercises
             </Button>
         )
     };
 }
+
+export default PdfButton;
